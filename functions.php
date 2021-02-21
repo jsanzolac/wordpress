6065467700fba273518b5b1c5591e13dad1a4b02
jsanzolac <52897067+jsanzolac@users.noreply.github.com>
2021-02-20 20:03:37 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@ add_action('after_setup_theme', 'init_template');
 
 function assets(){
     wp_register_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css', '', '5.0.0', 'all');
-    wp_register_style('font', 'https://fonts.googleapis.com/css2?family=Shippori+Mincho:wght@500&display=swap', '', '1.0', 'all'):
+    wp_register_style('font', 'https://fonts.googleapis.com/css2?family=Shippori+Mincho:wght@500&display=swap', '', '1.0', 'all');
     wp_enqueue_style('estilos', get_stylesheet_uri(), array('bootstrap', 'font'), '1.0', 'all');
 }
 
